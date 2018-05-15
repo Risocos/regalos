51d662e649ba16a617d9c283b11a23e786ff96c4
Romy Bos <romy.bos@hotmail.com>
2018-05-15 21:12:47 +0200

    Removed trash code, Changed layout to grid, Removed adds and some minor fixes

--- src/SingleProjectOverview.js
+++ src/SingleProjectOverview.js
@@ -1,7 +1,6 @@
 import React, {Component} from 'react';
-import {Form, Message, Header, Button, Image, Progress, Statistic} from "semantic-ui-react";
+import {Header, Button, Image, Progress, Statistic, Grid} from "semantic-ui-react";
 import "./SingleProjectOverview.css";
-import moment from 'moment';
 
 const options = [
     {key: 'f', text: 'Filter', value: 'filter'},
@@ -12,136 +11,54 @@ const options = [
 export class SingleProjectOverview extends Component {
     constructor(props) {
         super(props);
-
-        this.state = {
-            //Form values
-            name: '',
-            desc: '',
-            plan: '',
-            imagePath: '/white-image.png',
-            start: moment(),
-            end: moment(),
-            target: 0,
-            collabs: [],
-
-            //Utility variables
-            formMessage: [],
-        };
-
-        this.handleInputChange = this.handleInputChange.bind(this);
-        this.handleCollabInputChange = this.handleCollabInputChange.bind(this);
     }
-
-
-    handleInputChange(e) {
-        const value = e.target.value;
-        const name = e.target.name;
-        this.setState({
-            [name]: value,
-        });
-    }
-
-    handleCollabInputChange(event, text) {
-        this.setState({
-            collabs: text.value,
-        });
-    }
-
-    isANumber(string) {
-        return !isNaN(parseFloat(string)) && isFinite(string);
-    }
-
-    validate() {
-        let isValid = true;
-        let message = [];
-        if (this.state.name === '') {
-            message.push("Name is empty")
-            isValid = false;
-        }
-        if (this.state.desc === '') {
-            message.push("Description is empty")
-            isValid = false;
-        }
-        if (this.state.plan === '') {
-            message.push("Project plan is empty")
-            isValid = false;
-        }
-        if (this.state.target === 0) {
-            message.push("Target budget is empty")
-            isValid = false;
-        }
-
-
-        //Check if target budget is a number
-        if (!this.isANumber(this.state.target)) {
-            message.push("Target budget is not a number, please insert numbers only. Example: '10.45'")
-            isValid = false;
-        }
-
-        const errorMessage = (
-            <Message error>
-                <Message.Header>Oops! Something went wrong!</Message.Header>
-                <Message.List>
-                    {message.map((value) => <Message.Item key={value}>{value}</Message.Item>)}
-                </Message.List>
-            </Message>
-        );
-
-
-        this.setState({
-            formMessage: errorMessage
-        });
-
-        return isValid;
-
-    }
-
 
     render() {
 
         //TODO: funtionality of filters
         //TODO: Fill projectname, targetbudget etc with proper values
+        //TODO: Posts, phoyoalbum, see what we can do
         return (
             <div className="container">
-                <Form>
-                    <Form.Group widths='equal'>
-                        <Form.Group className='projectgroupside' grouped>
-                            <Image src='http://via.placeholder.com/300x1000'/>
-                        </Form.Group>
-                        <Form.Group className='projectgroupcenter' grouped>
-                            <Image src='http://via.placeholder.com/600x300'/>
+
+                <Grid columns='equal'>
+                    <Grid.Row>
+
+                        <Grid.Column>
+                            <p>Maybe recent updates, twitter messages about the project, photos, something like that?</p>
+                        </Grid.Column>
+
+                        <Grid.Column width={10}>
+                            <Image src='http://via.placeholder.com/600x300' centered='true'/>
                             <Header as='h2'>ProjectName</Header>
                             <Header as='h3'>TargetBudget: $10000</Header>
                             <Statistic>
                                 <Statistic.Value>5,550</Statistic.Value>
                                 <Statistic.Label>Donators!</Statistic.Label>
                             </Statistic>
-
-                                <Progress percent={83} progress success>
-                                    $8300
-                                </Progress>
-
-                            <p>A small project Description of the project. This will be like 6 sentences. This is some
-                                bullshit to see how it will look like.</p>
-                            <Header> <Button size='massive' color='green'>
-                                Donate now!
-                            </Button>
-                            </Header>
+                            <Progress percent={83} progress success>
+                                $8300
+                            </Progress>
+                            <p>Here will be the desciption of the project. It will be a max number of words to keep it small.</p>
+                            <Header> <Button size='massive' color='green'> Donate now! </Button> </Header>
                             <Button circular color='facebook' icon='facebook'/>
                             <Button circular color='twitter' icon='twitter'/>
                             <Button circular color='linkedin' icon='linkedin'/>
                             <Button circular color='google plus' icon='google plus'/>
                             <Button circular color='nstagram' icon='instagram'/>
+                            <Header size='huge'>Project details</Header>
+                            <p>A very detailed description. and maybe something with photoalbum</p>
+                            <Header size='huge'>Project progress</Header>
+                            <p>Project progress and smething with photoalbum and being able to make posts.</p>
+                        </Grid.Column>
 
-                            <p>A very detailed description. and maybe something with photos</p>
+                        <Grid.Column>
+                            <Header size="large">Info</Header>
+                            <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa strong. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo. </p>
+                        </Grid.Column>
+                    </Grid.Row>
 
-                        </Form.Group>
-                        <Form.Group className='projectgroupside' grouped>
-                            <Image src='http://via.placeholder.com/300x1000'/>
-                        </Form.Group>
-                    </Form.Group>
-
-                </Form>
+                </Grid>
             </div>
         )
     }
