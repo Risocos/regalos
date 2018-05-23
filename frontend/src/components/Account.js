d42b7518d4bb55b5ec3c6220f830c6a3a4017d18
Romy Bos <romy.bos@hotmail.com>
2018-05-23 22:25:32 +0200

    minor changes

--- frontend/src/components/Account.js
+++ frontend/src/components/Account.js
@@ -84,20 +84,14 @@ export class Account extends Component {
                     <Grid columns={4}>
                         <Grid.Row>
                             <Grid.Column>
-                                <Menu secondary vertical>
-                                    <Menu.Item name='account' active={activeItem === 'account'}
-                                               onClick={this.handleItemClick}/>
-                                    <Menu.Item name='settings' active={activeItem === 'settings'}
-                                               onClick={this.handleItemClick}/>
-                                    <Dropdown item text='Display Options'>
-                                        <Dropdown.Menu>
-                                            <Dropdown.Header>Text Size</Dropdown.Header>
-                                            <Dropdown.Item>Small</Dropdown.Item>
-                                            <Dropdown.Item>Medium</Dropdown.Item>
-                                            <Dropdown.Item>Large</Dropdown.Item>
-                                        </Dropdown.Menu>
-                                    </Dropdown>
-                                </Menu>
+                                    <Menu secondary vertical>
+                                        <Menu.Item name='account' active={activeItem === 'account'}
+                                                   onClick={this.handleItemClick}/>
+                                        <Menu.Item name='settings' active={activeItem === 'settings'}
+                                                   onClick={this.handleItemClick}/>
+                                        <Menu.Item name='projects' active={activeItem === 'projects'}
+                                                   onClick={this.handleItemClick}/>
+                                    </Menu>
                             </Grid.Column>
                             <Grid.Column>
                                 <Container text>
