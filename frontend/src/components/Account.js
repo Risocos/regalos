3e87ddda576ce0a24238416e81ac28fb4c2d3dea
Sander van Doorn <s.van.doorn@st.hanze.nl>
2018-06-01 01:42:03 +0200

    Removed a lot of unneeded code

--- frontend/src/components/Account.js
+++ frontend/src/components/Account.js
@@ -1,5 +1,5 @@
 import React, {Component} from "react";
-import {Button, Container, Grid, Header, Icon, Input, Item, Message, TextArea} from "semantic-ui-react";
+import {Button, Container, Grid, Header, Icon, Item} from "semantic-ui-react";
 import '../styling/Account.css';
 import axios from 'axios';
 import {ProjectCard} from "./ProjectCard";
@@ -8,15 +8,8 @@ export class Account extends Component {
 
     constructor(props) {
         super(props);
-        this.toggleEditMode = this.toggleEditMode.bind(this);
-        this.deleteAccount = this.deleteAccount.bind(this);
-        this.saveUser = this.saveUser.bind(this);
-        this.valueChanged = this.valueChanged.bind(this);
 
         this.state = {
-            editMode: false,
-            isDeleting: false,
-            isSaving: false,
             user: {
                 id: '',
                 email: '',
@@ -38,50 +31,27 @@ export class Account extends Component {
                     Authorization: TOKEN,
                 }
             }
-            ).then(res => {
-                const data = res.data.user;
-                this.setState({
-                    user: {
-                        email: data.email,
-                        username: data.username,
-                        image: 'http://via.placeholder.com/300x400',
-                        bio: data.bio,
-                        projects: data.projects,
-                    }
-                });
-            })
-    }
-
-    toggleEditMode() {
-        this.setState({editMode: !this.state.editMode});
-    }
-
-    deleteAccount() {
-        //if(confirm('Are you sure? There is no going back!')) {
-        this.setState({isDeleting: true});
-        setTimeout(() => {
-            alert('Your account is deleted');
-            this.setState({isDeleting: false});
-        }, 2000); // request delay simulation
-        //}
-    }
-
-    saveUser() {
-        // validate and send request
-        this.setState({isSaving: true});
-        setTimeout(() => {
-            this.setState({isSaving: false});
-            this.toggleEditMode();
-        }, 2000); // request delay simulation
+        ).then(res => {
+            const data = res.data.user;
+            this.setState({
+                user: {
+                    email: data.email,
+                    username: data.username,
+                    image: 'http://via.placeholder.com/300x400',
+                    bio: data.bio,
+                    projects: data.projects,
+                }
+            });
+        })
     }
 
     listProject(project) {
-        return(
+        return (
             <ProjectCard key={project.id}
-                id={project.id}
-                name={project.title}
-                desc={project.description}
-                target={project.target}
+                         id={project.id}
+                         name={project.title}
+                         desc={project.description}
+                         target={project.target}
             />)
     }
 
@@ -89,13 +59,37 @@ export class Account extends Component {
         return (
             <div>
                 <div className='account-header'>
-                    {this.state.editMode && <Message warning>You are in edit mode</Message>}
                     <Container text>
                         <Item.Group>
-                            {(this.state.editMode) ? this.renderEditForm() : this.renderNormal()}
+                            <Item>
+                                <Item.Image size='small'
+                                            src={this.state.user.image}/>
+                                <Item.Content>
+                                    <Item.Header as='h2'
+                                                 style={{paddingTop: "0px"}}>
+                                        {this.state.user.username}
+                                    </Item.Header>
+                                    <Item.Description>
+                                        <div>
+                                            <p>{this.state.user.bio}</p>
+                                        </div>
+                                    </Item.Description>
+                                    <Item.Extra>
+                                        <Button circular color='twitter' icon='twitter'/>
+                                        <Button circular color='linkedin' icon='linkedin'/>
+                                        <Button circular color='google plus' icon='google plus'/>
+                                        <div style={{float: 'right'}}>
+                                            <div>
+                                                <Button negative><Icon name='flag'/>Report user</Button>
+                                            </div>
+                                        </div>
+                                    </Item.Extra>
+                                </Item.Content>
+                            </Item>
                         </Item.Group>
                     </Container>
                 </div>
+
                 <Container style={{margin: '80px'}}>
                     <Header as='h1'>Projects</Header>
                     <Grid columns={3}>
@@ -105,64 +99,4 @@ export class Account extends Component {
             </div>
         )
     }
-
-    renderNormal() {
-        return (
-            <Item>
-                <Item.Image size='small'
-                            src={this.state.user.image}/>
-                <Item.Content>
-                    <Item.Header as='h2' style={{paddingTop: "0px"}}>{this.state.user.username}</Item.Header>
-                    <Item.Description>
-                        <div>
-                            <p>{this.state.user.bio}</p>
-                        </div>
-                    </Item.Description>
-                    <Item.Extra>
-                        <Button circular color='twitter' icon='twitter'/>
-                        <Button circular color='linkedin' icon='linkedin'/>
-                        <Button circular color='google plus' icon='google plus'/>
-                        <div style={{float: 'right'}}>
-                            <div>
-                                <Button negative><Icon name='flag'/>Report user</Button>
-                            </div>
-                        </div>
-                    </Item.Extra>
-                </Item.Content>
-            </Item>
-        )
-    }
-
-    renderEditForm() {
-        return (
-            <Item>
-                <Item.Image size='small'
-                            src={this.state.user.image} />
-                <Item.Content>
-                    <Input name='username' defaultValue={this.state.user.username} onChange={this.valueChanged} />
-                    <Item.Description>
-                        <TextArea name='bio' autoHeight value={this.state.user.bio} />
-                    </Item.Description>
-                    <Item.Extra>
-                        <Button circular color='twitter' icon='twitter'/>
-                        <Button circular color='linkedin' icon='linkedin'/>
-                        <Button circular color='google plus' icon='google plus'/>
-                        <div style={{float: 'right'}}>
-                            <Button icon color='green' labelPosition='left'
-                                    onClick={this.saveUser} loading={this.state.isSaving}>
-                                <Icon name='checkmark'/> Save
-                            </Button>
-                        </div>
-                    </Item.Extra>
-                </Item.Content>
-            </Item>
-        )
-    }
-
-    valueChanged = (e, { name, value }) => {
-        let u = this.state.user;
-        u[name] = value;
-        this.setState({ user: u });
-    }
-
 }
--- frontend/src/components/NavBar.js
+++ frontend/src/components/NavBar.js
@@ -12,15 +12,6 @@ export class NavBar extends Component {
             loggedIn: false,
             currentPage: 'home',
         };
-
-        this.handleClick = this.handleClick.bind(this);
-    }
-
-    handleClick(event, data) {
-        let nextPage = data.name;
-        this.setState({
-            currentPage: nextPage,
-        });
     }
 
 
@@ -32,12 +23,12 @@ export class NavBar extends Component {
                                to='/projects'
                                icon='calendar'
                                name='projects'
-                               onClick={this.handleClick}/>
+                    />
                     <Menu.Item as={Link}
                                to='/projects/create'
                                icon='add to calendar'
                                name='new project'
-                               onClick={this.handleClick}/>
+                    />
                 </Menu.Menu>
             );
         }
@@ -48,7 +39,7 @@ export class NavBar extends Component {
                                to='/projects'
                                icon='calendar'
                                name='projects'
-                               onClick={this.handleClick}/>
+                    />
                 </Menu.Menu>
             );
         }
@@ -78,11 +69,11 @@ export class NavBar extends Component {
                     <Menu.Item as={Link}
                                to='/login'
                                name='login'
-                               onClick={this.toggleLoggedIn}/>
+                     />
                     <Menu.Item as={Link}
                                to='/signup'
                                name='register'
-                               onClick={this.handleClick}/>
+                    />
                 </Menu.Menu>
             )
         }
