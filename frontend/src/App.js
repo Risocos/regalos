3cacc1ac10a32c5cb371f1aba93a05bab90018eb
Sander van Doorn <s.van.doorn@st.hanze.nl>
2018-06-01 22:41:11 +0200

    Fixed bug that prevented logged in users to get to create project page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -35,7 +35,8 @@ class App extends Component {
     }
 
     isAuthenticated() {
-        return (!sessionStorage.getItem("token").length>0)
+        console.log("auth");
+        return (!sessionStorage.getItem("token")>0)
     }
 
 
@@ -83,8 +84,8 @@ class App extends Component {
 
                                     <Route exact path="/projects/create"
                                            render={() => (
-                                               this.isAuthenticated ? (<Redirect to='/login'/>) :
-                                                   (props => <CreateProject basepath={BASEPATH}/>)
+                                               this.isAuthenticated() ? (<Redirect to='/login'/>) :
+                                                   (<CreateProject basepath={BASEPATH}/>)
                                            )}/>
                                     <Route exact path="/projects"
                                            render={props => <ProjectOverview basepath={BASEPATH}/>}/>
