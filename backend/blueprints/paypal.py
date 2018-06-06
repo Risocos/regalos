561c8ebc414fdd0bdb6b231033916e92744a577a
Melle Dijkstra <melle210202@gmail.com>
2018-06-06 13:40:56 +0200

    Begin paypal integration into regalos API

--- backend/blueprints/paypal.py
+++ backend/blueprints/paypal.py
@@ -15,6 +15,66 @@ def setup_paypal():
     })
 
 
+@paypal_api.route('/create-payment', methods=['POST'])
+def create_payment():
+    data = request.json
+
+    required_fields = [
+        'amount'
+    ]
+
+    if not data:
+        return jsonify({'message': 'No data given'}), 400
+
+    # TODO: put these validation rules in a marshmallow schema
+    for field in required_fields:
+        if field not in data:
+            return jsonify({'message': "Field '{field}' is not given".format(field=field)}), 422
+
+    try:
+        amount = float(data['amount'])
+        if not (amount > 0.01):
+            return jsonify({'message': "You need to donate more than 0.01"}), 422
+    except ValueError:
+        return jsonify({'message': "Invalid amount given"}), 422
+
+    payment = Payment({
+        'intent': 'sale',
+
+        'payer': {
+            'payment_method': 'paypal'
+        },
+
+        'redirect_urls': {
+            'return_url': 'http://localhost:5000/paypal/success',
+            'cancel_url': 'http://localhost:5000/paypal/cancel',
+        },
+
+        'transactions': [
+            {
+                'amount': {
+                    'total': '%.2f' % amount,
+                    'currency': 'EUR',
+                },
+                'description': "Regalos Project Donation.",
+            }
+        ]
+    })
+
+    if payment.create():
+        for link in payment.links:
+            if link.rel == 'approval_url':
+                return jsonify({
+                    'message': 'Paypal approval url created',
+                    'approval_url': str(link.href)
+                })
+    else:
+        return jsonify({
+            'message': 'Could not create paypal payment',
+            'error': payment.error
+        }), 409
+
+
 @paypal_api.route('/success')
 def success():
     if 'paymentId' in request.args:
